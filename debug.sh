bed9d576f90984eedf6ae6b6a6c01cdf339df804
tracnac <tracnac@devmobs.fr>
2020-05-11 22:23:57 +0200

    Add c89 stuff...

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
 rm -rf *.so *.o fe
 gcc -c -fPIC src/fe_math.c src/fe.c
 gcc -o fe_math.so -shared -fPIC fe_math.o fe.o -lm
-gcc -o fe src/fe.c -ldl -g
+gcc -o fe src/fe.c -ldl -Wall -Wextra -pedantic -std=c89 -march=native -g
