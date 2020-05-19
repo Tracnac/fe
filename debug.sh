a9b8f718239c8b4cf884fc741a6ab82b1030df5b
tracnac <tracnac@devmobs.fr>
2020-05-19 23:43:39 +0200

    Fix another stupid gnuism...

--- debug.sh
+++ debug.sh
@@ -5,7 +5,8 @@ export SRCDIR="${BASEDIR}/src"
 export BUILDDIR="${BASEDIR}/build"
 export MODULESDIR="${BASEDIR}/modules"
 export CC="gcc"
-export CFLAGS="-ldl -Wall -Wextra -pedantic -std=c89 -march=native -g"
+export CFLAGS="-Wall -Wextra -pedantic -std=c89 -march=native -Og"
+export LDFLAGS="-ldl"
 export LD_LIBRARY_PATH=${MODULESDIR}
 
 # Make directories
@@ -16,12 +17,12 @@ export LD_LIBRARY_PATH=${MODULESDIR}
 rm -rf ${BUILDDIR}/*.o ${MODULESDIR}/*.so fe
 
 # Make core
-${CC} -g -o ${BUILDDIR}/fe.o -c -fPIC ${SRCDIR}/fe.c
-${CC} -g -o ${MODULESDIR}/core.so -shared -fPIC -ldl ${BUILDDIR}/fe.o
+${CC} ${CFLAGS} -o ${BUILDDIR}/fe.o -c -fPIC ${SRCDIR}/fe.c
+${CC} ${CFLAGS} -o ${MODULESDIR}/core.so -shared -fPIC -ldl ${BUILDDIR}/fe.o
 
 # Make module
-${CC} -g -o ${BUILDDIR}/fe_math.o -c -fPIC ${SRCDIR}/fe_math.c
-${CC} -g -o ${MODULESDIR}/fe_math.so -shared -fPIC -lm -L${MODULESDIR} -l:core.so ${BUILDDIR}/fe_math.o
+${CC} ${CFLAGS} -o ${BUILDDIR}/fe_math.o -c -fPIC ${SRCDIR}/fe_math.c
+${CC} ${CFLAGS} -o ${MODULESDIR}/fe_math.so -shared -fPIC -lm -L${MODULESDIR} -l:core.so ${BUILDDIR}/fe_math.o
 
 # Make binaries
-${CC} ${CFLAGS} -o ${BASEDIR}/fe ${SRCDIR}/fe.c
+${CC} ${CFLAGS} -o ${BASEDIR}/fe ${SRCDIR}/fe.c ${LDFLAGS}
--- release.sh
+++ release.sh
@@ -5,7 +5,8 @@ export SRCDIR="${BASEDIR}/src"
 export BUILDDIR="${BASEDIR}/build"
 export MODULESDIR="${BASEDIR}/modules"
 export CC="gcc"
-export CFLAGS="-Os -ldl -Wall -Wextra -pedantic -std=c89 -march=native"
+export CFLAGS="-Os -Wall -Wextra -pedantic -std=c89 -march=native"
+export LDFLAGS="-ldl"
 export LD_LIBRARY_PATH=${MODULESDIR}
 
 # Make directories
@@ -16,14 +17,14 @@ export LD_LIBRARY_PATH=${MODULESDIR}
 rm -rf ${BUILDDIR}/*.o ${MODULESDIR}/*.so fe
 
 # Make core
-${CC} -Os -o ${BUILDDIR}/fe.o -c -fPIC ${SRCDIR}/fe.c
+${CC} ${CFLAGS} -o ${BUILDDIR}/fe.o -c -fPIC ${SRCDIR}/fe.c
 ${CC} -o ${MODULESDIR}/fe_core.so -shared -fPIC -ldl ${BUILDDIR}/fe.o
 
 # Make module
-${CC} -Os -o ${BUILDDIR}/fe_math.o -c -fPIC ${SRCDIR}/fe_math.c
+${CC} ${CFLAGS} -o ${BUILDDIR}/fe_math.o -c -fPIC ${SRCDIR}/fe_math.c
 ${CC} -o ${MODULESDIR}/fe_math.so -shared -fPIC -lm -L${MODULESDIR} -l:fe_core.so ${BUILDDIR}/fe_math.o
 for i in ${MODULESDIR}/*.so ; do strip -s ${i}; done 
 
 # Make binaries
-${CC} ${CFLAGS} -o ${BASEDIR}/fe ${SRCDIR}/fe.c
+${CC} ${CFLAGS} -o ${BASEDIR}/fe ${SRCDIR}/fe.c ${LDFLAGS}
 strip -s ${BASEDIR}/fe
